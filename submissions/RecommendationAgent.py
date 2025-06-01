96d88578c96aa4a97366252f413b6edfb16e3f69
sinegalatha <89790198+sinegalatha@users.noreply.github.com>
2025-06-01 14:25:28 +0530

    Update RecommendationAgent.py

--- submissions/RecommendationAgent.py
+++ submissions/RecommendationAgent.py
@@ -4,7 +4,7 @@ from read_env import *
 
 def recommendationTool(topic,proficiency_level):
     query = 'Give me the best study materials in terms of youtube tutorials and websites for the give topic to get profiency in the given proficiency level. Topic:' + topic +'Profiency Level:' + proficiency_level
-    agent = Agent(model=AzureOpenAI(id="supplychain-gpt-4o",
+    agent = Agent(model=AzureOpenAI(id="XXXXXXXX",
                                api_key=AZURE_OPENAI_API_KEY), markdown=True)
     assistant_message = agent.run(query).messages[-1].content
     return(assistant_message)
